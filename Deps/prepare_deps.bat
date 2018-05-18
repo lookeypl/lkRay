8d3345f725bde00281ce94cd7511cd0dd973042b
Lukasz Kostyra <costyrra.xl@gmail.com>
2018-05-18 18:36:59 +0200

    Integrate lkCommon under Windows

---
+++ Deps/prepare_deps.bat
@@ -0,0 +1,47 @@
+@echo off
+
+REM TODO:
+REM  * Discover CPU count
+
+echo | set /p=Checking for cmake... 
+for %%X in (cmake.exe) do (set FOUND=%%~$PATH:X)
+if defined FOUND (
+    echo FOUND
+) else (
+    echo NOT FOUND
+    echo     Make sure you download CMake before using this script.
+    pause
+    exit /b
+)
+
+echo | set /p=Checking for msbuild... 
+for %%X in (msbuild.exe) do (set FOUND=%%~$PATH:X)
+if defined FOUND (
+    echo FOUND
+) else (
+    echo NOT FOUND
+    echo     Make sure you set MSBuild on your PATH variable before using this script.
+    pause
+    exit /b
+)
+
+
+pushd .
+
+cd %~dp0
+echo Current directory is %cd%
+
+REM ========== lkCommon build ===========
+
+cd lkCommon
+
+msbuild lkCommon.sln /t:lkCommon:Rebuild /p:Configuration=Debug;Platform=x86
+msbuild lkCommon.sln /t:lkCommon:Rebuild /p:Configuration=Release;Platform=x86
+msbuild lkCommon.sln /t:lkCommon:Rebuild /p:Configuration=Debug;Platform=x64
+msbuild lkCommon.sln /t:lkCommon:Rebuild /p:Configuration=Release;Platform=x64
+
+echo.
+echo Script is done
+echo.
+
+popd
--- lkRay/Main.cpp
+++ lkRay/Main.cpp
@@ -1,37 +1,50 @@
 #include "PCH.hpp"
 
+#include <lkCommon/Utils/Logger.hpp>
 #include <lkCommon/System/Window.hpp>
 #include <lkCommon/Utils/Timer.hpp>
-#include <iostream>
+#include <lkCommon/Utils/Image.hpp>
 
 lkCommon::Window gWindow;
 
-int WINDOW_WIDTH = 1280;
-int WINDOW_HEIGHT = 720;
+uint32_t WINDOW_WIDTH = 1280;
+uint32_t WINDOW_HEIGHT = 720;
 
 int main()
 {
     gWindow.Init();
     if (!gWindow.Open(300, 300, WINDOW_WIDTH, WINDOW_HEIGHT, "lkRay"))
     {
+        LOGE("Failed to open window");
         return -1;
     }
 
     gWindow.SetTitle("lkRay");
 
+    lkCommon::Image result;
+    if (!result.Resize(WINDOW_WIDTH, WINDOW_HEIGHT))
+    {
+        LOGE("Failed to resize result Image");
+        return -1;
+    }
+
     lkCommon::Timer t;
     t.Start();
     while (gWindow.IsOpen())
     {
-        gWindow.Update(t.Stop());
+        gWindow.Update(static_cast<float>(t.Stop()));
 
-        for (int i = 0; i < WINDOW_WIDTH; ++i)
+        for (uint32_t x = 0; x < WINDOW_WIDTH; ++x)
         {
-            for (int j = 0; j < WINDOW_HEIGHT; ++j)
+            for (uint32_t y = 0; y < WINDOW_HEIGHT; ++y)
             {
-                //gWindow.SetPixel(i, j, (i << 16) | j);
+                float r = static_cast<float>(x) / static_cast<float>(WINDOW_WIDTH) * 255.0f;
+                float b = static_cast<float>(y) / static_cast<float>(WINDOW_HEIGHT) * 255.0f;
+                result.SetPixel(x, y, static_cast<uint8_t>(r), static_cast<uint8_t>(0), static_cast<uint8_t>(b));
             }
         }
+
+        gWindow.DisplayImage(0, 0, result);
     }
 
     return 0;
